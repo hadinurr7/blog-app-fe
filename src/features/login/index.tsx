4d490e44a63c62cb276ace9757c464d47ded92b4
hadinurr7 <153978165+hadinurr7@users.noreply.github.com>
2024-12-09 14:24:08 +0700

    keeplogin (#3)

--- src/features/login/index.tsx
+++ src/features/login/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import { LoginSchema } from "./schema";
 import { useFormik } from "formik";
-import useLogin from "@/api/hooks/auth/useLogin";
+import useLogin from "@/hooks/api/auth/useLogin";
 
 const LoginPage = () => {
   const { mutateAsync: login, isPending } = useLogin();
--- src/hooks/api/auth/useLogin.ts
+++ src/hooks/api/auth/useLogin.ts
@@ -1,8 +1,11 @@
 "use client";
 
 import { axiosInstance } from "@/lib/axios";
+import { useAppDispatch } from "@/redux/hooks";
+import { loginAction } from "@/redux/slices/userSlice";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
 interface LoginPayload {
@@ -11,13 +14,21 @@ interface LoginPayload {
 }
 
 const useLogin = () => {
+
+  const router = useRouter()
+
+  const dispatch = useAppDispatch()
+
   return useMutation({
     mutationFn: async (payload: LoginPayload) => {
       const { data } = await axiosInstance.post("/auth/Login", payload);
       return data;
     },
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Login success");
+      dispatch(loginAction(data))
+      localStorage.setItem("blog-storage", JSON.stringify(data))
+      router.replace("/")
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data);
